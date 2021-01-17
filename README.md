a4a3deaf670b37f84f5f5f4dbb327d07a304aa46
andersonchau <59462609+andersonchau@users.noreply.github.com>
2021-01-17 16:30:12 +0800

    router added

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
   <!-- ABOUT THE PROJECT -->
-  Demo for mastermind game simulation using Angular and other function demo
+  Demo for mastermind game simulation using Angular and other functions for learning purpose
 
 
 
 1. basic component - service - UI interaction(gamemap.component/GameService.ts)
 2. http get JSON from JSON server (display.component/api-client-service.service.ts)
-
+3. Simple Router : app.component.html
   <!-- Spring Boot Items -->
 
   ![alt text](https://github.com/andersonchau/guess_game_angular/blob/main/screenshot.png?raw=true)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GamemapComponent } from './gamemap/gamemap.component';
+
+
 import { FormsModule } from '@angular/forms';
-import { DisplaytableComponent } from './displaytable/displaytable.component';
+
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { DisplaytableComponent } from './displaytable/displaytable.component';
+import { GamemapComponent } from './gamemap/gamemap.component';
+
+const appRoutes: Routes = [
+  { path: 'playmastermind', component: GamemapComponent },
+  { path: 'showtable', component: DisplaytableComponent },
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +29,11 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
